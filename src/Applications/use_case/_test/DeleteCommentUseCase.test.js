bf70814363ebd4dab449fa7980ba94bab95d2b8b
mbengvw <mbeng@uniku.ac.id>
2023-12-21 11:13:21 +0700

    fix deleteCommentUseCase test

--- src/Applications/use_case/_test/DeleteCommentUseCase.test.js
+++ src/Applications/use_case/_test/DeleteCommentUseCase.test.js
@@ -1,3 +1,61 @@
-test('adds 1 + 2 to equal 3', () => {
-  expect(3).toBe(3);
+/* eslint-disable comma-dangle */
+const DeleteCommentUseCase = require('../DeleteCommentUseCase');
+const DeleteComment = require('../../../Domains/comments/entities/DeleteComment');
+const CommentRepository = require('../../../Domains/comments/CommentRepository');
+const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
+
+describe('DeleteCommentUsecase', () => {
+  it('should orchestrating the delete comment action correctly', async () => {
+    // Arrange
+    const useCasePayload = {
+      id: 'this is a comment',
+      owner: 'user-123',
+      threadId: 'thread-321',
+    };
+
+    /** creating dependency of use case */
+    const mockThreadRepository = new ThreadRepository();
+    const mockCommentRepository = new CommentRepository();
+
+    /** mocking needed function */
+    mockThreadRepository.isThreadExist = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve(true));
+
+    mockCommentRepository.isCommentExist = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve(true));
+
+    mockCommentRepository.isCommentOwner = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve(true));
+
+    mockCommentRepository.deleteComment = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve(true));
+
+    /** creating use case instance */
+    const deleteCommentUseCase = new DeleteCommentUseCase({
+      commentRepository: mockCommentRepository,
+      threadRepository: mockThreadRepository,
+    });
+
+    // Action
+    await deleteCommentUseCase.execute(useCasePayload);
+
+    // Assert
+    expect(mockThreadRepository.isThreadExist).toHaveBeenCalledWith(
+      useCasePayload.threadId
+    );
+    expect(mockCommentRepository.isCommentExist).toHaveBeenCalledWith(
+      useCasePayload.id
+    );
+    expect(mockCommentRepository.isCommentOwner).toHaveBeenCalledWith(
+      useCasePayload.id,
+      useCasePayload.owner
+    );
+    expect(mockCommentRepository.deleteComment).toHaveBeenCalledWith(
+      useCasePayload.id
+    );
+  });
 });
